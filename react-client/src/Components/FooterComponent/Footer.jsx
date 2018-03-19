2ec575b63a307f3b7ede2f0f294c33825c939c20
Gabe Villasana <villagab4@gmail.com>
2018-03-19 00:46:00 +0000

    Styling

---
+++ react-client/src/Components/FooterComponent/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+	render() {
+		return (
+			<footer>
+				<div>
+					Fotter component
+				</div>
+			</footer>
+		)
+	}
+}
+
+export default Footer;
